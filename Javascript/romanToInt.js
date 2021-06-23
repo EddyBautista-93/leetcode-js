ccb7c3a96f12a32158f3866cade89e2005b83746
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-23 06:02:00 -0500

    completed romanToInt leetcode question

--- Javascript/romanToInt.js
+++ Javascript/romanToInt.js
@@ -16,3 +16,31 @@
 // X can be placed before L (50) and C (100) to make 40 and 90. 
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
+
+const romanToInt = (s) => {
+    // set up value rules
+    let result = 0;
+    const length = s.length;
+    if(s == null) return 0;
+
+    var myMap = new Map();
+    myMap.set('I', 1);
+    myMap.set('V', 5);
+    myMap.set('X', 10);
+    myMap.set('L', 50);
+    myMap.set('C', 100);
+    myMap.set('D', 500);
+    myMap.set('M', 1000);
+
+    for (let i = 0; i < length; i++) {
+        if(myMap.get(s.charAt(i)) < myMap.get(s.charAt(i + 1))){
+            result -= myMap.get(s.charAt(i))
+        } else {
+          result += myMap.get(s.charAt(i))
+        }               
+    }
+    return result; 
+}
+console.log(romanToInt('VI'));
+console.log(romanToInt('M'));
+console.log(romanToInt('VII'));
