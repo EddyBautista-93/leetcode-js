7b1e7351cb1995d58edd26e9f56554d3f9ba7a52
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-28 16:42:17 -0500

    completed question

--- Javascript/ValidParentheses.js
+++ Javascript/ValidParentheses.js
@@ -4,7 +4,7 @@
 
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
- 
+
 
 // Example 1:
 
@@ -26,23 +26,33 @@
 
 // Input: s = "{[]}"
 // Output: true
- 
+
 
 // Constraints:
 
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
 
-const isValid = (inputString) => { 
-    var charParenthesesArray = inputString.split('');
-
-    if(charParenthesesArray.includes('(')){
-    
+const isValid = (s) => {
+    var stack = []; 
+    var len = s.length;
+    var map = {
+        '(': ')',   // Use a map to help
+        '[': ']',   // with referencing 
+        '{': '}'    // the brackets.
+    };
+    for (let i in s) {
+        if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+            stack.pop(); 
+        } else {
+            stack.push(s[i]); // put the element into the stack
+        }
     }
-    charParenthesesArray.forEach(element => {
-        console.log(element);
-    });
-    return null;
-}
+    return stack.length === 0;
+};
 
-isValid("()")
+isValid("()") // true
+isValid("()[]{}") // true
+isValid("(]") // false 
+isValid("([)]") // false 
+isValid("{[]}") // true
